93d4c4ae2b622adfd20b82312fddee35f66bfe3f
yumi <z1181806270@gmail.com>
2025-11-15 01:22:41 +0800

    feat: 更新路由配置并移除表格相关组件
    - 添加用户管理和酒店管理的路由，包括房间管理、酒店前台、折扣管理和活动管理
    - 移除不再使用的表格列表及其相关组件（CreateForm 和 UpdateForm）
    - 清理代码以提升可读性

--- frontend/config/routes.ts
+++ frontend/config/routes.ts
@@ -15,7 +15,21 @@ export default [
       { path: '/admin/sub-page', name: '二级管理页', component: './Admin' },
     ],
   },
-  { name: '查询表格', icon: 'table', path: '/list', component: './table-list' },
+  { name: '用户管理', icon: 'table', path: '/user-manage', component: './user-manage' },
+  {
+    path: '/hotel',
+    name: '酒店管理',
+    icon: 'dashboard',
+    routes: [
+      { path: '/hotel', redirect: '/hotel/room-manage' },
+      { name: '房间管理', icon: 'table', path: '/hotel/room-manage', component: './hotel/room-manage' },
+      { name: '酒店前台', icon: 'table', path: '/hotel/hotel-front', component: './hotel/hotel-front' },
+      { name: '折扣管理', icon: 'table', path: '/hotel/discount-manage', component: './hotel/discount-manage' },
+      { name: '活动管理', icon: 'table', path: '/hotel/activity-manage', component: './hotel/activity-manage' },
+    ],
+  },
+  { name: '人员管理', icon: 'table', path: '/staff-manage', component: './staff-manage' },
+  { name: '系统设置', icon: 'table', path: '/system-setting', component: './system-setting' },
   { path: '/', redirect: '/welcome' },
   { path: '*', layout: false, component: './404' },
 ];
